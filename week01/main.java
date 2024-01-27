c92edb5fc5e786e2e37fa121a356dcd9cf02a54f
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-01-27 14:23:42 +0400

    Greeted the user using println and name with print

--- week01/main.java
+++ week01/main.java
@@ -2,8 +2,11 @@ package week01;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
+        System.out.println("Dear User, nice to meet with you");
+        System.out.print("My name is Mehdi Shukurlu");
+
+
 
 
     }
