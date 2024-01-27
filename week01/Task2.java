48073698665470541c51ffa00cb9430b0c0fd299
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-01-27 15:33:17 +0400

    Used scanner to sum up 3 random integers.

--- week01/Task2.java
+++ week01/Task2.java
@@ -1,5 +1,14 @@
 package week01;
+import java.util.*;
 
 public class Task2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
+        int sum = a + b + c;
+
+        System.out.println(sum);
+
+        scanner.close();
+}
 }
