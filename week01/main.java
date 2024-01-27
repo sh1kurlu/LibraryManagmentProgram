5f3e187925a724a95e1f4346eaa8e3146090709b
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-01-27 15:05:31 +0400

    explained package week01 and public class main

--- week01/main.java
+++ week01/main.java
@@ -1,11 +1,14 @@
 package week01;
+// Package declaration indicating that this class belongs to the 'week01' package
 
+
+// This Main class named as "main"
 public class main {
     public static void main(String[] args) {
         
         System.out.println("Dear" + args[0] +", nice to meet with you");
         System.out.print("My name is" + args[0]);
-        
+
 
 
     }
