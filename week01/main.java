21ff868a64fbb04faa21213a1818d038ec4b5cdb
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-01-27 15:19:33 +0400

    Explained the rest parts of the program

--- week01/main.java
+++ week01/main.java
@@ -5,9 +5,23 @@ package week01;
 // This Main class named as "main"
 public class main {
     public static void main(String[] args) {
+        /*
+        Main method:
+
+        public - method can be accessed from anywhere
+        static - allows the method to be called without creating an instance of the class
+        void - return type of 'main' which indicates that the method does not 'return' a value 
+        main - a name that is given as a starting method of the program
+        String[] args - an array of strings that can be used to pass command-line arguments to the program if the user wants.
+
+        */
         
+        //Used to print a welcome message using the first command line argument
         System.out.println("Dear" + args[0] +", nice to meet with you");
-        System.out.print("My name is" + args[0]);
+        
+        // Used to print a message introducing the program's user using second command line argument
+        System.out.println("My name is" + args[0]);
+
 
 
 
