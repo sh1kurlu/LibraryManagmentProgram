2f793c3ae644defcc0df4aaa11551489a6b6ef00
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-02 13:47:11 +0400

    Added Digits.java to find sum, product and average of its numbers + used printf to format the decimals

---
+++ week02/Digits.java
@@ -0,0 +1,32 @@
+package week02;
+import java.util.*;
+
+public class Digits {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        double sum = 0;
+        int product = 1;
+
+        int count = 0;
+
+        while(num > 0){
+            sum += num%10;
+            product *= num%10;
+            num /= 10;
+
+            count++;
+            
+    
+
+        }
+
+        double average = sum/count;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.printf("Average: %.1f", (average));
+    }
+
+}
