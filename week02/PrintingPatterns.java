ef7db9f4d66e7189a13b742ce3d524bfafe9f0c0
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-03 15:50:43 +0400

    Added PrintingPatterns.java and make patterns with '*' and '#'

--- week02/PrintingPatterns.java
+++ week02/PrintingPatterns.java
@@ -5,18 +5,38 @@ public class PrintingPatterns {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int size = s.nextInt();
+        String[][] pattern = new String[size][size];
 
-        for(int i = 1; i<size-1; i++){
-                System.out.println("*#");
-                for(int j = 1; j<size-1; j++){
-                    System.out.print("*#");
+        for(int i = 0; i<size-1; i++){
+            for(int j = 0; j<size; j++){
+                if((i+j)%2==0){
+                    pattern[i][j] = "*";
                 }
-            
-        }
+                else{
+                    pattern[i][j] = "#";
+                }
+            }
+                }
 
+                for(int i = 0; i<size-1; i++){
+                    for(int j = 0; j<size; j++){
+                        System.out.print(pattern[i][j]);
+
+                    }
+
+                    System.out.println();
+                        }
+                
+            
+            
+                s.close();
+            }
+    
+        
+     
     }
     
     
     
     
-}
+
