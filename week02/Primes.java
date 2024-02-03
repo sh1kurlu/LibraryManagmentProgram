4e4d604dfa3e6d1af88e535c229d0b54e850ed56
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-03 16:41:23 +0400

    Added Primes.java to figure out whether given number is prime or not

---
+++ week02/Primes.java
@@ -0,0 +1,28 @@
+package week02;
+import java.util.*;
+
+
+public class Primes{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(), count = -1;
+        
+
+        for(int i = 1; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                count++;
+            }
+            
+        }
+
+        if(count == 0){
+            System.out.println("Its a prime");
+        }
+        else{
+            System.out.println("Its not a prime!");
+        }
+        
+    }
+
+    
+}
