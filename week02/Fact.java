c40a9b14c785c6e82cee5b8c7c615c5302a15354
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-03 11:55:15 +0400

    Modified Fact.java to detect negative numbers and freeze the operation

--- week02/Fact.java
+++ week02/Fact.java
@@ -6,18 +6,26 @@ public class Fact {
        Scanner s = new Scanner(System.in);
        int n = s.nextInt();
        int result = 1;
-
-       if(n == 1){
-        System.out.println(1);
+       if(n >= 0 ){
+        if(n == 1 || n == 0){
+            System.out.println(1);
+           }
+           
+          
+           else{
+            for(int i = 1; i<=n; i++){
+                result = result * i;
+            }
+            System.out.println(result);
+           }
        }
-       
-      
        else{
-        for(int i = 1; i<=n; i++){
-            result = result * i;
-        }
-       }
+        System.out.println("Negative number error!");
 
-       System.out.println(result);
+       }
+  
+    
+
+       
     }
 }
