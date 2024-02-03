12589f99f868b8040ca11888ec778df3d76feffa
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-03 11:47:04 +0400

    Added PolarityOfNumber.java and to check the polarity using command-line argument

--- week02/PolarityOfNumber.java
+++ week02/PolarityOfNumber.java
@@ -2,6 +2,17 @@ package week02;
 
 public class PolarityOfNumber {
     public static void main(String[] args) {
-        
+        double num = Double.parseDouble(args[0]);
+
+        if(num > 0){ 
+            System.out.println("Number is positive");
+        }
+        else if (num < 0){
+            System.out.println("Number is negative");
+        }
+
+        else { 
+            System.out.println(0);
+        }
     }
 }
