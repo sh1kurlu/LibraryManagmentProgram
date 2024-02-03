5254f6495792edaa706da3f4d27f66b791fe413c
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-03 11:40:46 +0400

    Added LeapYear.java used Scanner as an user input

---
+++ week02/LeapYear.java
@@ -0,0 +1,17 @@
+package week02;
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int year = s.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            System.out.println("Its a leap year!");
+
+        }
+        else{
+            System.out.println("Its not a leap year");
+        }
+    }
+}
