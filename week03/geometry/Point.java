8324a9096807ae3c4a45919e9ebe17627ad14f5b
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-09 13:36:53 +0400

    Added geometry file + Segment and Point classes

---
+++ week03/geometry/Point.java
@@ -0,0 +1,16 @@
+package week03.geometry;
+
+public class Point {
+    float x,y;
+     
+    public float Point(){
+      return x;
+    }
+
+    public float Point(float x, float y){
+        return y;
+    }
+
+    
+}
+
---
+++ week03/geometry/Segment.java
@@ -0,0 +1,5 @@
+package week03.geometry;
+
+public class Segment {
+    
+}
