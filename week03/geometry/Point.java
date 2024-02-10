e54a9447bf737dd69c0f545413dfc5c615fd8a56
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-10 13:20:28 +0400

    Added getY and setY

--- week03/geometry/Point.java
+++ week03/geometry/Point.java
@@ -1,15 +1,45 @@
 package week03.geometry;
 
 public class Point {
-    float x,y;
+    private float x,y;
      
-    public float Point(){
-      return x;
+    public Point(){
+      
     }
 
-    public float Point(float x, float y){
-        return y;
+    public Point(float x, float y){
+        this.x = x;
+        this.y = y;
     }
+
+    public Point(Point p){
+        this.x = p.x;
+        this.y = p.y;
+    }
+    public float getX(){
+        return x;
+    }
+
+    public void setX(float x){
+        this.x = x;
+    }
+
+    public float getY(){
+        return x;
+    }
+
+    public void setY(float y){
+        this.y = y;
+    }
+
+    public void translate(float dX, float dY){
+        this.x = this.x + dX;
+        this.y = this.y + dY;
+    }
+
+    
+
+
 
     
 }
--- week03/geometry/Segment.java
+++ week03/geometry/Segment.java
@@ -1,5 +1,15 @@
 package week03.geometry;
 
 public class Segment {
-    
+    private Point p1,p2;
+
+    public Segment(Point p1, Point p2){
+        this.p1 = new Point(p1);
+        this.p2 = new Point(p2);
+    }
+
+    public Segment(float x1, float y1, float x2, float y2){
+        
+
+    } 
 }
