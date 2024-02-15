9919dbb49b1819616cf75538709cab4b0ecc7422
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-16 01:27:40 +0400

    Added week4 and SortingString problem with using Array package

---
+++ week04/SortingStrings.java
@@ -0,0 +1,23 @@
+package week04;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingStrings {
+
+    public static String toSort(String input) {
+        char[] sorted = input.toLowerCase().toCharArray();
+        Arrays.sort(sorted);
+        return new String(sorted);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        String sortedText = toSort(input);
+
+        System.out.println("Sorted Text: " + sortedText);
+
+        s.close();
+    }
+}
