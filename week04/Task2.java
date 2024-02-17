810eea9d48570080bb6380b1302585df913b37ce
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-17 22:53:49 +0400

    Added method to print both Min and Max of input array

--- week04/Task2.java
+++ week04/Task2.java
@@ -1,6 +1,9 @@
 package week04;
+import java.util.*;
 
 public class Task2 {
+
+
     private static int[] parseArgumentsToIntArray(String[] args) {
         int [] numbers = new int[args.length];
         for(int i = 0; i<args.length; i++){
@@ -34,9 +37,32 @@ public class Task2 {
         return max;
     }
 
-    
+    private static int[]FindMinAndMax(int [] numbers){
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        for(int number: numbers){
+
+            if(number < min){
+                min = number;
+            }
+            if(number > max){
+                max = number;
+            }
+        }
+
+        return new int[]{min,max};
+    }
 
 
+    private static void PrintMinAndMax(int [] numbers){
+        int min = FindMin(numbers);
+        int max =FindMax(numbers);
+
+        System.out.println(min);
+        System.out.println(max);
+
+    }
 
 
 
