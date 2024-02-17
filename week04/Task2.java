92999cc67555b9e3fa7d128240500b48e6d12e8b
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-17 22:46:08 +0400

    Added task2.java and added methods to return max and min values of argument

--- week04/Task2.java
+++ week04/Task2.java
@@ -1,5 +1,43 @@
 package week04;
 
 public class Task2 {
+    private static int[] parseArgumentsToIntArray(String[] args) {
+        int [] numbers = new int[args.length];
+        for(int i = 0; i<args.length; i++){
+            numbers[i] += Integer.parseInt(args[i]);
+        }
+
+        return numbers;
+    }
+
+    private static int FindMin(int [] numbers){
+        int min = Integer.MIN_VALUE;
+
+        for(int number: numbers){
+        if(number < min){
+            min = number;
+        }
+    }
+    return min;
+
+    }
+
+    private static int FindMax(int [] numbers){
+        int max = Integer.MAX_VALUE;
+
+        for(int number: numbers){
+           if(max > number){
+            max = number;
+           }
+        }
+
+        return max;
+    }
+
     
+
+
+
+
+
 }
