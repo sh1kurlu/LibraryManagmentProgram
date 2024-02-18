076489273ed6d12f959f5972f4a9292e7fa42d02
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-18 20:04:23 +0400

    Added task15.java imported invoice class from invoice week03

--- week04/Task15.java
+++ week04/Task15.java
@@ -1,5 +1,23 @@
 package week04;
-
+import java.util.Scanner;
+import week03.invoice.Invoice;
 public class Task15 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberOfInvoices =scan.nextInt();
+        scan.nextLine();
+        Invoice[] invoices = new Invoice[numberOfInvoices];
+        for(int i = 0; i<numberOfInvoices; i++){
+            String partNumber = scan.nextLine();
+            String description = scan.nextLine();
+            int quantityOfItem = scan.nextInt();
+            double pricePerItem = scan.nextDouble();
+            scan.nextLine();
+            invoices[i] = new Invoice(partNumber, description, quantityOfItem, pricePerItem);
+        }
+        for(int i = 0; i<numberOfInvoices; i++){
+            System.out.println(invoices[i].getInvoiceAmount());
+        }
+        scan.close();
+    }
 }
