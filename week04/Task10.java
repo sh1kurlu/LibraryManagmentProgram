047595681f0d0e58ea08e80112765e0ece30b021
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-18 16:14:52 +0400

    Added Task10 imported array package + used lowercase to get rid of minor issues

--- week04/Task10.java
+++ week04/Task10.java
@@ -1,5 +1,29 @@
 package week04;
+import java.util.Arrays;
 
 public class Task10 {
-    
+    public static void main(String[] args) {
+        String str1 = "Alasm";
+        String str2 = "Salam";
+
+        boolean areAnagrams = areAnagrams(str1, str2);
+        System.out.println("Result: " + areAnagrams);
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
 }
