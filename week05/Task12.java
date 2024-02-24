28f9ecf5ecbfafc3c75bf10c74446a1033536655
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-24 21:25:10 +0400

    Added Task12.java as a continuation for Task11

--- week05/Task12.java
+++ week05/Task12.java
@@ -1,5 +1,19 @@
 package week05;
 
+class Square extends Rectangle{
+    int side;
+    public Square(int a){
+        super(a, a);
+        a = side;
+    }
+}
 public class Task12 {
-    
+    public static void main(String[] args) {
+        Object o1 = new Rectangle(5,10);
+        Object o2 = new Rectangle(15,15);
+        Object o3 = new Square(15);
+        System.out.println(o1.equals(o2));
+        System.out.println(o1.equals(o3));
+        System.out.println(o2.equals(o3));
+    }
 }
