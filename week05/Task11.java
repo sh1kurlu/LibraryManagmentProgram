ec3a597861eced599b278808fb347b1537a72fa5
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-24 21:20:55 +0400

    Added Task11.java

---
+++ week05/Task11.java
@@ -0,0 +1,30 @@
+package week05;
+class Rectangle {
+    int width, height;
+    public Rectangle(int w, int h){
+        width = w;
+        height = h;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        Rectangle rect = (Rectangle) obj;
+        if(rect.width == width && rect.height == height) {       
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+public class Task11 {
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(5,10);
+        Rectangle r2 = new Rectangle(15,10);
+        Rectangle r3 = new Rectangle(5,10);
+        System.out.println(r1.equals(r2));
+        System.out.println(r1.equals(r3));
+        System.out.println(r2.equals(r3));
+    }
+}
