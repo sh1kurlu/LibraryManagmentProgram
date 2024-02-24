f5f808b066c3848cee72a225d5566ffef66e326a
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-02-24 14:46:26 +0400

    Added + updated Task7.java

--- week05/Task7.java
+++ week05/Task7.java
@@ -1,6 +1,35 @@
 package week05;
 
-public class Task7{
-    
+class MathDemo{
+    public int min(int a, int b){
+        int min = (a < b)? a : b;
+        return min;
     }
+    public int max(int a, int b){
+        int max = (a > b)? a : b;
+        return max;
+    }
+
+    public int sum(int args[]){
+        int sum = 0;
+        for(int num: args){
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    public float mean(int [] args){
+        return sum(args)/args.length;
+    }
+
+    public int factorial(int n){
+        if(n == 0 || n == 1 ) return 1;
+        if(n<0) return -1;
+        
+        return n * factorial(n-1);
+        
+    }
+}
+
 
