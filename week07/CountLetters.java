1b25697638931f189e3aab36dfcdcc071ec9a5f4
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-03-09 23:55:49 +0400

    Modified the countletters so that any unexpected word would be ignored using exception handling

--- week07/CountLetters.java
+++ week07/CountLetters.java
@@ -1,17 +1,16 @@
 package week07;
-
 import java.util.Scanner;
 
 public class CountLetters {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] counts = null;
+        int[] counts;
         String word;
 
         do {
             counts = new int[26];
-            System.out.print("Enter a word, preferrably only english letters (0 to exit): ");
+            System.out.print("Enter a word, preferably only English letters (0 to exit): ");
             word = scan.nextLine();
 
             word = word.toLowerCase();
@@ -19,14 +18,24 @@ public class CountLetters {
             if (word.trim().equals("0"))
                 break;
 
-            for (int i = 0; i < word.length(); i++)
-                counts[word.charAt(i) - 'a']++;
+            try {
+                for (int i = 0; i < word.length(); i++) {
+                    char currentChar = word.charAt(i);
+                    if (Character.isLetter(currentChar)) {
+                        counts[currentChar - 'a']++;
+                    }
+                }
 
-            System.out.println("\tHere are the letters:");
+                System.out.println("\tHere are the letters:");
 
-            for (int i = 0; i < counts.length; i++)
-                if (counts[i] != 0)
-                    System.out.println((char) (i + 'a') + ": " + counts[i]);
+                for (int i = 0; i < counts.length; i++) {
+                    if (counts[i] != 0) {
+                        System.out.println((char) (i + 'a') + ": " + counts[i]);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter only English letters!");
+            }
 
         } while (true);
 
