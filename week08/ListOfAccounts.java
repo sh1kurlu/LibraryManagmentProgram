99404f92eb9f5e9d5f084230162408a03c48a9bb
sh1kurlu <mshukurlu19525@ada.edu.az>
2024-03-17 01:42:28 +0400

    Added ListOfAccount.java

---
+++ week08/ListOfAccounts.java
@@ -0,0 +1,74 @@
+package week08;
+import java.util.*;
+
+class Account {
+    private String firstname;
+    private String lastname;
+    private double balance;
+
+    public Account(String firstname, String lastname, double balance) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public String getFullname() {
+        return firstname + " " + lastname;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class ListOfAccounts {
+    public static void main(String[] args) {
+        // Create a list of Accounts
+        List<Account> accountList = new ArrayList<>();
+
+        // Add elements to the list
+        accountList.add(new Account("Mehdi", "Shukurlu", 3000.0));
+        accountList.add(new Account("Cavid", "Aghayev", 2000.0));
+        accountList.add(new Account("Ramil", "Zohrabli", 2500.0));
+        accountList.add(new Account("Kamal", "Mammadov", 1000.0));
+
+        // Print the elements
+        System.out.println("Original List:");
+        for (Account account : accountList) {
+            System.out.println(account);
+        }
+
+        Collections.sort(accountList, new Comparator<Account>() {
+            @Override
+            public int compare(Account account1, Account account2) {
+                return account1.getFullname().compareTo(account2.getFullname());
+            }
+        });
+
+        System.out.println("\nSorted by fullname:");
+        for (Account account : accountList) {
+            System.out.println(account);
+        }
+
+        Collections.sort(accountList, new Comparator<Account>() {
+            @Override
+            public int compare(Account account1, Account account2) {
+                return Double.compare(account1.getBalance(), account2.getBalance());
+            }
+        });
+
+        System.out.println("\nSorted by balance:");
+        for (Account account : accountList) {
+            System.out.println(account);
+        }
+    }
+}
